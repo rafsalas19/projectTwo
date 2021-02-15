826af990a0a7a882c9d4d0a4f7642fe24d6760e3
Rafael <rafsalas1987@hotmail.com>
2021-02-14 19:31:26 -0600

    added more functionality to the shell like access checking and parsing

--- shell/wish.c
+++ shell/wish.c
@@ -4,34 +4,77 @@
 #include <assert.h>
 #include <string.h>
 #include <sys/stat.h>
+#include <unistd.h>
 
 #define VECTOR_SIZE 100
 
-int vectorSize= VECTOR_SIZE;
+int pathVectorSize= VECTOR_SIZE;
 char **pathVector;
+char curDir[]="~/";
+
 int currentIndex =0;
+//int *pathSizeVector;
+
+
 
 
 int resizedVector(){
-	char** temp = (char**)malloc(sizeof(char*)*2*vectorSize);
-	if (temp ==NULL){
+	char** temp = (char**)malloc(sizeof(char*)*2*pathVectorSize);
+	//int* tempSize= (int*)malloc(sizeof(int)*2*pathVectorSize);
+	
+	if (temp ==NULL ){
 		fprintf(stderr, "Failed to resize pathVector array.\n");
 		exit(1);
 	}
-	memcpy(temp, pathVector, vectorSize*sizeof(char**));
-	vectorSize=2*vectorSize;
+	memcpy(temp, pathVector, pathVectorSize*sizeof(char**));
+	//memcpy(tempSize, pathSizeVector, pathVectorSize*sizeof(int*));
+	pathVectorSize=2*pathVectorSize;
 	pathVector =temp;
+	//pathSizeVector = tempSize;
+	//tempSize=NULL;
 	temp=NULL;
 	return 0;
 }
 
 
-int checkAccess(){
-	
+int isBuiltIn(char* command,int length){return 0;}
+
+char* checkAccess(char* command,int length){
+	int ret = access(command, X_OK);
+	if (ret==0) {return 0;}
 	for(int i=0;i<currentIndex;++i){
-		int return access("/bin/ls", X_OK);
+		int size =  strlen(command)+strlen(pathVector[i])+1;
+		char* fullpath = (char*)malloc(sizeof(char)*size);
+		//char slash ='/';
+		
+		memcpy(fullpath,pathVector[i] , strlen(pathVector[i])*sizeof(char));
+		fullpath[strlen(pathVector[i])]='/';
+		//memcpy(fullpath+pathSizeVector*sizeof(char),&slash , sizeof(char));
+		memcpy(fullpath+strlen(pathVector[i])+1, command , strlen(command)*sizeof(char));
+		
+		ret = access(fullpath, X_OK);
+		if (ret==0) {return 0;}
 	}
-	return -1;
+	return NULL;
+}
+
+void parseAndExecute(char* cmdLine){
+	
+	
+	char * token;
+	while( ( token= strtok(cmdLine," ")) != NULL ){
+        //printf("%s\n",token);
+		
+		if(strcmp(cmdLine,"exit") ==0 ||strcmp(cmdLine,"exit\n") ==0){
+			printf("\n");
+			exit(0);
+		}	
+		
+	}
+	
+	
+
+	
 }
 
 
@@ -40,17 +83,25 @@ int main(int argc, char*argv[]){
 	char *buffer;
 	size_t length=0; 
 	
-	path = (char**)malloc(sizeof(char*)*pathVectorSize);
+	pathVector = (char**)malloc(sizeof(char*)*pathVectorSize);
+	//pathSizeVector = (int*)malloc(sizeof(int)*pathVectorSize);
+	
+
+	// char *tmp = (char*)malloc(sizeof(char)*5);
+	// //print("fst size:%d \n",strlen(tmp));
+	// for(int i=0;i<5;++i){
+		
+		// tmp[i] = 'a';
+	// }
+	// printf("fst size:%ld \n",strlen(tmp));
+	// tmp[4]='\0';
+	// printf("snd size:%ld \n",strlen(tmp));
 	
 	if(argc<2){
 		while(1){
 			printf("wish>");
 			getline(&buffer,&length,stdin);
-			
-			if(strcmp(buffer,"exit\n") ==0 ){
-				printf("\n");
-				exit(0);
-			}
+			parseAndExecute(buffer);
 		}
 	}
 	
